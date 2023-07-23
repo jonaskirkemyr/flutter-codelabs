2430e9fd69e686b4e385550a29d7d3d65f675c73
Jonas Kirkemyr <jonas@kirkemyr.no>
2023-07-23 10:17:49 +0200

    remove standard widgets from project creation
    replaced with widgets as described from guide

---
+++ lib/data.dart
@@ -0,0 +1,30 @@
+import "dart:convert";
+
+const documentJSON = '''
+{
+  "metadata": {
+    "title": "My Document",
+    "modified": "2023-05-10"
+  },
+  "blocks": [
+    {
+      "type": "h1",
+      "text": "Chapter 1"
+    },
+    {
+      "type": "p",
+      "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    },
+    {
+      "type": "checkbox",
+      "checked": false,
+      "text": "Learn Dart 3"
+    }
+  ]
+}
+''';
+
+class Document {
+  final Map<String, Object?> _json;
+  Document() : _json = jsonDecode(documentJSON);
+}
