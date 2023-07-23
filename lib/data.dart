0212ed28446becacfae2f2c38888a33357c4da87
Jonas Kirkemyr <jonas@kirkemyr.no>
2023-07-23 10:49:36 +0200

    read json using map pattern

--- lib/data.dart
+++ lib/data.dart
@@ -30,15 +30,14 @@ class Document {
   Document() : _json = jsonDecode(documentJSON);
 
   (String, {DateTime modified}) get metadata {
-    if (_json.containsKey("metadata")) {
-      final metadataJson = _json["metadata"];
-
-      if (metadataJson is Map) {
-        final title = metadataJson["title"];
-        final localModified = DateTime.parse(metadataJson["modified"]);
-
-        return (title, modified: localModified);
-      }
+    if (_json
+        case {
+          "metadata": {
+            "title": String title,
+            "modified": String localModified,
+          }
+        }) {
+      return (title, modified: DateTime.parse(localModified));
     }
 
     throw const FormatException("Unexpected JSON");
