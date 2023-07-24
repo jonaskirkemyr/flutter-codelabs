db48afefc1e32fd2517fa06efd84b88f46f14f00
Jonas Kirkemyr <jonas@kirkemyr.no>
2023-07-24 21:54:43 +0200

    add widget for displaying star_gazers

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import "star_counter.dart";
 
 void main() {
   runApp(const StarCounterApp());
@@ -60,7 +61,9 @@ class _HomePageState extends State<HomePage> {
                     ),
                     Padding(
                       padding: const EdgeInsets.only(top: 32.0),
-                      child: Text(_repositoryName),
+                      child: GitHubStarCounter(
+                        repositoryName: _repositoryName,
+                      ),
                     ),
                   ],
                 )),
